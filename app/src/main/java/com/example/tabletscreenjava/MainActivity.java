5abe16040acc9eb087ab0eb64d98d0cc090facd2
alexw <awolf@vwd.com>
2021-01-14 12:57:02 +0100

    added REST API call function for dummy server

--- app/src/main/java/com/example/tabletscreenjava/MainActivity.java
+++ app/src/main/java/com/example/tabletscreenjava/MainActivity.java
@@ -3,15 +3,29 @@ package com.example.tabletscreenjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import static com.example.tabletscreenjava.changeRoom.EXTRA_TEXT_ChangeRoom;
 
@@ -19,16 +33,52 @@ public class MainActivity extends AppCompatActivity {
 
     public TextView currentDayTextView;
     public Button changeRoom_Btn;
+    public TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Elements
         currentDayTextView = findViewById(R.id.currentDay_textView);
         changeRoom_Btn = findViewById(R.id.changeRoom_Btn);
-        changeRoom_Btn.setOnClickListener(v -> openChangeRoom());
+        resultTextView = findViewById(R.id.textView_result);
 
+        Retrofit retroFit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceholderApi jsonPlaceholderApi = retroFit.create(JsonPlaceholderApi.class);
+
+        Call<List<Post>> call = jsonPlaceholderApi.getPosts();
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if(!response.isSuccessful()){
+                    resultTextView.setText("Code: " + response.code());
+                }
+
+                List<Post> posts = response.body();
+
+                for(Post post : posts){
+                    String content = "";
+                    content += "ID: " + post.getId() + "\n";
+                    content += "User ID: " + post.getUserId() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+                    content += "Text: " + post.getText() + "\n\n";
+                    resultTextView.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                resultTextView.setText(t.getMessage());
+            }
+        });
+
+        changeRoom_Btn.setOnClickListener(v -> openChangeRoom());
         currentDayTextView.setText(getCurrentDate());
         String example = getIntent().getStringExtra(EXTRA_TEXT_ChangeRoom);
         changeRoom_Btn.setText(example);
